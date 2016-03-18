3d2fba65a99d908fc03bab61cd5cd4e597732ee4
Josh Washke <jwashke@icloud.com>
2016-03-17 22:17:48 -0600

    Adding extensions

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,8 +5,10 @@ SECRET_NUMBER = rand(100)
 
 get '/' do
   guess = params["guess"]
-  message = check_guess(guess)
-  erb :index, :locals => { :message => message }
+  cheat = params["cheat"]
+  message, color = check_guess(guess)
+  message = cheat_mode(message) if cheat
+  erb :index, :locals => { :message => message, :color => color }
 
 end
 
@@ -14,14 +16,18 @@ def check_guess(guess)
   guess = guess.to_i
   return "Please guess a number" unless guess
   if guess > SECRET_NUMBER + 5
-    "Your guess of #{guess} is way too high!"
+    ["Your guess of #{guess} is way too high!", "red"]
   elsif guess > SECRET_NUMBER
-    "Your guess of #{guess} is too high!"
+    ["Your guess of #{guess} is too high!", "salmon"]
   elsif guess < SECRET_NUMBER - 5
-    "Your guess of #{guess} is way too low!"
+    ["Your guess of #{guess} is way too low!", "red"]
   elsif guess < SECRET_NUMBER
-    "Your guess of #{guess} is too low!"
+    ["Your guess of #{guess} is too low!", "salmon"]
   else
-    "You got it right! The secret number is #{SECRET_NUMBER}"
+    ["You got it right! The secret number is #{SECRET_NUMBER}", "green"]
   end
+end
+
+def cheat_mode(message)
+  "#{message} The Secret Number is #{SECRET_NUMBER}"
 end
